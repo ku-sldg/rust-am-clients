c79aa67f30a3a4fca763f0d1cb306c7b00a44d33
Adam Petz <ampetz@ku.edu>
2025-04-08 18:22:55 -0500

    adding more CL args and better default values (now relative to environment variable)

--- executables/rust-am-client/src/main.rs
+++ executables/rust-am-client/src/main.rs
@@ -1,11 +1,14 @@
 // main.rs (rust-am-client)
 
+//#![allow(non_camel_case_types)]
+//#![allow(non_snake_case)]
+
 // Custom package imports
 use rust_am_lib::copland::*;
 use rust_am_lib::copland::EvidenceT::*;
-use rust_am_lib::copland::FWD::*;
-use rust_am_lib::copland::EvInSig::*;
-use rust_am_lib::copland::EvOutSig::*;
+//use rust_am_lib::copland::FWD::*;
+//use rust_am_lib::copland::EvInSig::*;
+//use rust_am_lib::copland::EvOutSig::*;
 //use rust_am_lib::copland::ASP::*;
 //use rust_am_lib::copland::Term::*;
 //use rust_am_lib::copland::SP::ALL;
@@ -24,59 +27,53 @@ fn main() -> std::io::Result<()> {
     let args = get_am_client_args()?;
 
     let term_filepath : String = args.term_filepath;
-    eprintln!("\n\nterm_filepath arg: {}\n\n", term_filepath);
+    println!("\nterm_filepath arg: {}", term_filepath);
 
     let att_server_uuid_string : String = args.server_uuid;
-    eprintln!("\n\nserver_uuid arg: {}\n\n", att_server_uuid_string);
+    println!("server_uuid arg: {}", att_server_uuid_string);
+
+    let plcmap_filepath : String = args.plcmap_filepath;
+    println!("plcmap_filepath arg: {}", plcmap_filepath);
+
+    let glob_type_env_filepath : String = args.env_filepath;
+    println!("env_filepath arg: {}", glob_type_env_filepath);
+
+    let glob_comps_filepath : String = args.glob_comps_filepath;
+    println!("glob_comps_filepath arg: {}", glob_comps_filepath);
+
 
     let term_contents = fs::read_to_string(term_filepath).expect("Couldn't read Term JSON file");
-    eprintln!("Term contents:\n{term_contents}");
+    eprintln!("\nTerm contents:\n{term_contents}");
 
-    #[allow(non_snake_case)]
     let t : Term = serde_json::from_str(&term_contents)?;
-    println!("Decoded Term as: \n");
-    println!("\t{:?}\n", t); // :? notation since formatter uses #[derive(..., Debug)] trait
+    println!("\nDecoded Term as:");
+    println!("{:?}", t); // :? notation since formatter uses #[derive(..., Debug)] trait
 
-    //let my_nonce : String = "nonce_val".to_string();  //hex::encode("PASSED".to_string());
-    let rawev_vec = vec![]; //vec![my_nonce];
-    let my_plcmap: HashMap<Plc, String> = 
-        HashMap::from([("P0".to_string(), "localhost:5000".to_string()), 
-                       ("P1".to_string(), "localhost:5001".to_string()), 
-                       ("P2".to_string(), "localhost:5002".to_string())
-                       ]);
-    let my_pubmap: HashMap<Plc, String> = HashMap::from([]);
+    let plcmap_contents = fs::read_to_string(plcmap_filepath).expect("Couldn't read plcmap JSON file");
 
-    let attest_evsig : EvSig = 
-            EvSig { FWD: EXTEND,
-                    EvInSig: ALL, 
-                    EvOutSig: OutN("1".to_string()) };
+    let my_plcmap: HashMap<Plc, String> = serde_json::from_str(&plcmap_contents)?;
+    eprintln!("\nDecoded plcmap as:");
+    eprintln!("{:?}", my_plcmap);
 
-    let appraise_evsig : EvSig = 
-    EvSig { FWD: EXTEND,
-            EvInSig: ALL, 
-            EvOutSig: OutN("1".to_string()) };
+    let glob_type_env_contents = fs::read_to_string(glob_type_env_filepath).expect("Couldn't read glob_type_env JSON file");
 
-    let certificate_evsig : EvSig = 
-    EvSig { FWD: EXTEND,
-            EvInSig: ALL, 
-            EvOutSig: OutN("1".to_string()) };
+    let my_glob_type_env: HashMap<ASP_ID, EvSig> = serde_json::from_str(&glob_type_env_contents)?;
+    eprintln!("\nDecoded glob_type_env as:");
+    eprintln!("{:?}", my_glob_type_env);
 
-    let magic_appr_evsig : EvSig = 
-    EvSig { FWD: REPLACE,
-            EvInSig: ALL, 
-            EvOutSig: OutN("1".to_string()) };
+    let glob_comps_contents = fs::read_to_string(glob_comps_filepath).expect("Couldn't read glob_comps JSON file");
 
-    let my_glob_type_env : HashMap<ASP_ID, EvSig> = HashMap::from ([("attest".to_string(), attest_evsig), 
-                                                                    ("appraise".to_string(), appraise_evsig), 
-                                                                    ("certificate".to_string(), certificate_evsig), 
-                                                                    ("magic_appr".to_string(), magic_appr_evsig)]);
-    let my_glob_comps : HashMap<ASP_ID, ASP_ID> = HashMap::from([("attest".to_string(), "magic_appr".to_string()),
-                                                                 ("appraise".to_string(), "magic_appr".to_string()),
-                                                                 ("certificate".to_string(), "magic_appr".to_string())]);
+    let my_glob_comps: HashMap<ASP_ID, ASP_ID> = serde_json::from_str(&glob_comps_contents)?;
+    eprintln!("\nDecoded glob_comps as:");
+    eprintln!("{:?}", my_glob_comps);
 
     let my_glob_context : GlobalContext = 
             GlobalContext { ASP_Types: my_glob_type_env, 
                             ASP_Comps: my_glob_comps};
+
+    let my_pubmap: HashMap<Plc, String> = HashMap::from([]);
+
+    let rawev_vec = vec![]; //vec![my_nonce];
 
     let my_evidence : Evidence = 
             Evidence { RAWEV: RawEv::RawEv (rawev_vec),
@@ -88,38 +85,29 @@ fn main() -> std::io::Result<()> {
                                   PubKey_Mapping: my_pubmap, 
                                   Session_Context: my_glob_context };
 
-    /*
-    let cert_asp_params: ASP_PARAMS = 
-                ASP_PARAMS { ASP_ID: "attest".to_string(),
-                ASP_ARGS: serde_json::Value::Bool(true), 
-                ASP_PLC: "P0".to_string(),
-                ASP_TARG_ID: "cert_targ".to_string() };
-    */
-
-    let my_term: Term = t; 
-    //asp (ASPC (cert_asp_params));//t; //asp (ASPC (cert_asp_params));//(ASPC (ALL, EXTD("1".to_string()), cert_asp_params));   //asp(SIG); //t; 
+    let my_term: Term = t;  
     let vreq : ProtocolRunRequest = 
         ProtocolRunRequest {
             TYPE: "REQUEST".to_string(), 
             ACTION: "RUN".to_string(), 
             REQ_PLC: "TOP_PLC".to_string(), 
             TERM: my_term,
-            EVIDENCE: my_evidence, //RawEv(rawev_vec),
+            EVIDENCE: my_evidence,
             ATTESTATION_SESSION: my_att_session};
 
-    let req_str = serde_json::to_string(&vreq)?;//encode_gen(&vreq)?;
+    let req_str = serde_json::to_string(&vreq)?;
 
     let stream = connect_tcp_stream(att_server_uuid_string)?;
-    println!("\nTrying to send ProtocolRunRequest JSON string: \n");
+    println!("\nTrying to send ProtocolRunRequest: \n");
     println!("{req_str}\n");
 
     let resp_str = am_sendRec_string(req_str,&stream)?;
-    println!("Got a TCP Response String: \n");
-    println!("\t{resp_str}\n");
+    eprintln!("Got a TCP Response String: \n");
+    eprintln!("{resp_str}\n");
 
     let resp : ProtocolRunResponse = serde_json::from_str(&resp_str)?;
-    println!("Decoded ProtocolRunResponse as: \n");
-    println!("{:?}\n", resp); // :? notation since formatter uses #[derive(..., Debug)] trait
+    println!("Decoded ProtocolRunResponse: \n");
+    println!("{:?}\n", resp);
 
     Ok (())
 }
--- lib/src/clientArgs.rs
+++ lib/src/clientArgs.rs
@@ -1,23 +1,75 @@
 use clap::Parser;
 
-
+const DEFAULT_TERM_PATH: &'static str = "/testing/cert_appr.json";
 const DEFAULT_SERVER_UUID: &'static str = "localhost:5000";
+const DEFAULT_PLCMAP_PATH: &'static str = "/testing/plcmap_default.json";
+const DEFAULT_TYPE_ENV_PATH: &'static str = "/testing/glob_type_env_default.json";
+const DEFAULT_GLOB_COMPS_PATH: &'static str = "/testing/glob_comps_default.json";
+
+const AM_CLIENTS_ENV_VAR: &'static str = "AM_CLIENTS_ROOT";
+
+fn get_local_env_var(s:String) -> std::io::Result<String> {
+
+    let env_var_string = match std::env::var(&s) {
+        Ok(val) => val,
+        Err(_e) => {
+            panic!("Did not set environment variable {}\n", s)
+        }
+    };
+
+    Ok (env_var_string)
+
+}
+
+fn get_local_env_var_w_suffix (env_var_string:String, suffix:&str) -> std::io::Result<String> {
+
+    let env_var_val = get_local_env_var(env_var_string)?;
+
+    let owned_env_string = env_var_val.to_owned();
+    let owned_suffix_string = suffix.to_owned();
+
+    let together = format!("{owned_env_string}{owned_suffix_string}");
+
+    Ok (together)
+}
 
 // Adapted from:  https://docs.rs/clap/latest/clap/
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct AmClientArgs {
     /// Path pointing to (JSON) protocol term file
-    #[arg(short, long)]
+    #[arg(short, long , default_value_t = 
+        get_local_env_var_w_suffix(AM_CLIENTS_ENV_VAR.to_string(), 
+                                   DEFAULT_TERM_PATH).expect("Couldn't initialize default value for term_filepath field of AmClientArgs struct.  
+                                                              Check for missing Environment Variable?"))]
     pub term_filepath: String,
 
     /// UUID string for AM server destination
     #[arg(short, long , default_value_t = DEFAULT_SERVER_UUID.to_string())]
-    pub server_uuid: String
+    pub server_uuid: String,
+    
+    /// Path pointing to (JSON) Placemap file
+    #[arg(short, long , default_value_t = 
+        get_local_env_var_w_suffix(AM_CLIENTS_ENV_VAR.to_string(), 
+                                   DEFAULT_PLCMAP_PATH).expect("Couldn't initialize default value for plcmap_filepath field of AmClientArgs struct.  
+                                                                Check for missing Environment Variable?"))]
+    pub plcmap_filepath: String,
 
+    /// Path pointing to (JSON) ASP GLOBAL TYPE ENVIRONMENT file
+    #[arg(short, long , default_value_t =
+        get_local_env_var_w_suffix(AM_CLIENTS_ENV_VAR.to_string(), 
+                                   DEFAULT_TYPE_ENV_PATH).expect("Couldn't initialize default value for env_filepath field of AmClientArgs struct.  
+                                                                  Check for missing Environment Variable?"))]
+    pub env_filepath: String,
+
+     /// Path pointing to (JSON) ASP GLOBAL ASP COMPAT MAP file
+     #[arg(short, long , default_value_t =
+        get_local_env_var_w_suffix(AM_CLIENTS_ENV_VAR.to_string(), 
+                                   DEFAULT_GLOB_COMPS_PATH).expect("Couldn't initialize default value for glob_comps_filepath field of AmClientArgs struct.  
+                                                                    Check for missing Environment Variable?"))]
+     pub glob_comps_filepath: String
 }
 
-#[allow(non_snake_case)]
 pub fn get_am_client_args () -> std::io::Result<AmClientArgs> {
     let args: AmClientArgs = AmClientArgs::parse();
     Ok(args)
